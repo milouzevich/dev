ab2298429778d65429ea08bc88aac2ab6d55a0b1
user <user@debian>
2017-09-27 14:33:14 +0200

    reorg repertoire

---
+++ web/PHP/php_latex/creation_fichier_latex/ReadMe.md
@@ -0,0 +1,3 @@
+# Objectif : Générer un document .tex avec un minimum de balise php
+
+#### ici il n'y a que les balises d'entête php. Tout le reste du document sera construit uniquement avec les balises LaTeX.
---
+++ web/PHP/php_latex/creation_fichier_latex/latex_sans_php.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: application/x-latex');
+header('Content-Disposition: attachment; filename="test_latex.tex"');
+?>
+\documentclass{article}
+\begin{document}
+Mise en place dans doc latex sans echo de php...\newline
+Version permettant d'evoluer vers le template comme smarty\newline
+\end{document}
