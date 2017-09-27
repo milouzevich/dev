9457af08cf137728ed1dcb2ec8ed37934bc55f2d
user <user@debian>
2017-09-27 14:35:16 +0200

    delete fichier doublon

--- web/PHP/php_latex/ReadMe.md
+++
@@ -1,3 +0,0 @@
-# Objectif : Générer un document .tex avec un minimum de balise php
-
-#### ici il n'y a que les balises d'entête php. Tout le reste du document sera construit uniquement avec les balises LaTeX.
--- web/PHP/php_latex/latex_sans_php.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/x-latex');
-header('Content-Disposition: attachment; filename="test_latex.tex"');
-?>
-\documentclass{article}
-\begin{document}
-Mise en place dans doc latex sans echo de php...\newline
-Version permettant d'evoluer vers le template comme smarty\newline
-\end{document}
